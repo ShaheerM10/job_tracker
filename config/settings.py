7c74f5a4ab12a8679d2fd2e207536120d975c320
Shaheer Mahmood <shaheermahmood@Shaheers-MacBook-Pro.local>
2026-05-17 16:39:42 -0700

    Add Railway domain to ALLOWED_HOSTS

--- config/settings.py
+++ config/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-change-in-production')
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*', 'jobtracker-production-b988.up.railway.app']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
