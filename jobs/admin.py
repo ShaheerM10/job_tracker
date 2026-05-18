3673eb8a0fa158426e1df2f7bf536157197d388b
Shaheer Mahmood <shaheermahmood@Shaheers-MacBook-Pro.local>
2026-05-17 17:29:06 -0700

    Register JobApplication in admin

--- jobs/admin.py
+++ jobs/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import JobApplication
 
-# Register your models here.
+@admin.register(JobApplication)
+class JobApplicationAdmin(admin.ModelAdmin):
+    list_display = ['job_title', 'company', 'user', 'status', 'applied_date']
+    list_filter = ['status', 'applied_date']
+    search_fields = ['job_title', 'company', 'user__email']
+    ordering = ['-applied_date']
